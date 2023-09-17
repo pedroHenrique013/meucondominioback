db9db3149df21a10bf3beafce151110b0b22b2c1
unknown <sdstanisci@gmail.com>
2023-09-16 21:31:34 -0300

    ajuste cors

--- src/main/java/br/com/impacta/meucondominio/application/config/WebConfig.java
+++ src/main/java/br/com/impacta/meucondominio/application/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://allowed-origin.com")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
     }
 }
